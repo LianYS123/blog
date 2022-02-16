56385a9e356c7a0a8f08089b6d1adb74c3cedf60
LianYS123 <50662988+LianYS123@users.noreply.github.com>
2022-02-16 10:31:04 +0800

    移除主页无用内容,移动端内容居中

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,62 +11,24 @@ import {
   useMediaQuery,
   useTheme
 } from "@mui/material";
-import { Box } from "@mui/system";
-import { SkeletonList } from "components/skeleton";
-import { useFetchList } from "hooks";
-import { Article } from "pages/article/Article";
 import React from "react";
-import { ARTICLE_LIST } from "services/article";
 import Masonry from "@mui/lab/Masonry";
 import { Article as ArticleIcon, GitHub, Web } from "@mui/icons-material";
 import { PROJECT_ITEMS } from "./config";
 
 const Home = () => {
-  const {
-    list = [],
-    loadingFirstPage,
-    loadingMore
-  } = useFetchList({
-    service: ARTICLE_LIST
-  });
   const theme = useTheme();
   const upSM = useMediaQuery(theme.breakpoints.up("sm"));
 
   return (
-    <Container className="md:pb-16 p-4">
-      {/* Banner */}
-      <div className="mb-8 rounded-md overflow-hidden">
-        <Box sx={{ height: { xs: "40vw", sm: "30vw" }, position: "relative" }}>
-          <img
-            className="w-full h-full object-cover object-center"
-            src="https://liuli-1259462774.cos.ap-shanghai.myqcloud.com/c39d7a8e-90fb-4ec3-9db4-6eb4d728ff92cat-6747298_1280.jpeg"
-          />
-          <div className="absolute left-3 bottom-2 sm:left-6 sm:bottom-4 text-white">
-            {upSM ? (
-              <Typography variant="body2" component="div">
-                记录生活、技术和有趣的事
-              </Typography>
-            ) : (
-              <Typography variant="body1" component="div">
-                记录生活、技术和有趣的事
-              </Typography>
-            )}
-          </div>
-        </Box>
-      </div>
-
+    <Container>
       {/* 项目列表 */}
-
       <div className="px-2">
-        <div className="mb-4">
-          <Typography variant="h6" gutterBottom>
-            我的项目
-          </Typography>
-          <Typography className="font-light" variant="body1">
-            做过一些有趣的项目
-          </Typography>
-        </div>
-        <Masonry columns={{ xs: 1, sm: 2, md: 4 }} spacing={2}>
+        <Masonry
+          style={{ margin: upSM ? undefined : 0 }}
+          columns={{ xs: 1, sm: 2, md: 4 }}
+          spacing={2}
+        >
           {PROJECT_ITEMS.map((item, index) => (
             <Card key={index}>
               <CardHeader title={item.title} subheader={item.subTitle} />
@@ -105,26 +67,6 @@ const Home = () => {
             </Card>
           ))}
         </Masonry>
-      </div>
-
-      <div className="h-16"></div>
-      {/* 文章 */}
-
-      {/* <div className="mb-4">
-        <Typography variant="h6" gutterBottom>
-          文章
-        </Typography>
-        <Typography className="font-light" variant="body1">
-          文章推荐
-        </Typography>
-      </div> */}
-
-      <div className="space-y-3 mb-4 px-4">
-        <SkeletonList loading={loadingFirstPage} />
-        {list.map(it => (
-          <Article key={it.id} {...it} />
-        ))}
-        <SkeletonList loading={loadingMore} />
       </div>
     </Container>
   );
