700c1fc4f88f9617150a3810f812788dc02e2374
LianYS123 <50662988+LianYS123@users.noreply.github.com>
2022-03-07 20:09:37 +0800

    修改脚本

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-ssh -T lian 'rm -rf /home/lian/static/blog-snowy-browser/*'
-scp output/blog.zip lian:/home/lian/static/blog-snowy-browser/
-ssh lian 'cd /home/lian/static/blog-snowy-browser/ && unzip -q blog.zip'
+STATIC_PATH=/home/www/static/blog-snowy-browser/
+
+ssh -T tianyi 'rm -rf '${STATIC_PATH}'*'
+scp output/blog.zip tianyi:${STATIC_PATH}
+ssh tianyi 'cd '${STATIC_PATH}' && unzip -q blog.zip'
