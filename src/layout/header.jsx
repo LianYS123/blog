882198fad92fdd3beb70e47fe4fd39e9043c49e5
LianYS123 <50662988+LianYS123@users.noreply.github.com>
2022-03-10 17:44:18 +0800

    下滑隐藏导航栏

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -10,54 +10,57 @@ import { useWindowScroll } from "react-use";
 import { useHistory, useLocation } from "react-router-dom";
 import routers from "routers";
 import { APP_MENUS } from "./config";
-import { ButtonBase } from "@mui/material";
+import { ButtonBase, Slide, useScrollTrigger } from "@mui/material";
 
 export default function AppHeader() {
   const { y } = useWindowScroll();
   const history = useHistory();
   const { pathname } = useLocation();
+  const trigger = useScrollTrigger();
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        color="inherit"
-        sx={{ boxShadow: y > 10 ? 1 : 0 }}
-        position="fixed"
-      >
-        <Toolbar>
-          <SideBarMenuButton />
-          <Typography
-            className="cursor-pointer"
-            variant="h6"
-            component="div"
-            onClick={() => history.push(routers.HOME)}
-            sx={{ flexGrow: 1 }}
-          >
-            Lian's Space
-          </Typography>
-          <div className="space-x-4 mr-2 hidden sm:block">
-            {APP_MENUS.map(({ to, text, icon }) => {
-              return (
-                <ButtonBase
-                  key={text}
-                  onClick={() => history.push(to)}
-                  sx={{
-                    px: 0.8,
-                    py: 0.4,
-                    borderBottom: to === pathname ? "3px solid" : "none",
-                    borderColor: "primary.main"
-                  }}
-                >
-                  {text}
-                </ButtonBase>
-              );
-            })}
-          </div>
-          <div className="flex items-center">
-            <ThemeSwitch />
-            <UserAvatar />
-          </div>
-        </Toolbar>
-      </AppBar>
+      <Slide in={!trigger}>
+        <AppBar
+          color="inherit"
+          sx={{ boxShadow: y > 10 ? 1 : 0 }}
+          position="fixed"
+        >
+          <Toolbar>
+            <SideBarMenuButton />
+            <Typography
+              className="cursor-pointer"
+              variant="h6"
+              component="div"
+              onClick={() => history.push(routers.HOME)}
+              sx={{ flexGrow: 1 }}
+            >
+              Lian's Space
+            </Typography>
+            <div className="space-x-4 mr-2 hidden sm:block">
+              {APP_MENUS.map(({ to, text, icon }) => {
+                return (
+                  <ButtonBase
+                    key={text}
+                    onClick={() => history.push(to)}
+                    sx={{
+                      px: 0.8,
+                      py: 0.4,
+                      borderBottom: to === pathname ? "3px solid" : "none",
+                      borderColor: "primary.main"
+                    }}
+                  >
+                    {text}
+                  </ButtonBase>
+                );
+              })}
+            </div>
+            <div className="flex items-center">
+              <ThemeSwitch />
+              <UserAvatar />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </Slide>
     </Box>
   );
 }
