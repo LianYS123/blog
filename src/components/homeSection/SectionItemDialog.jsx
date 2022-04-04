0e50d6697c72e30c42cbd5e2c82f1f8f8ba2e6cd
LianYS123 <50662988+LianYS123@users.noreply.github.com>
2022-04-04 10:19:15 +0800

    添加展开类型

--- src/components/homeSection/SectionItemDialog.jsx
+++ src/components/homeSection/SectionItemDialog.jsx
@@ -32,16 +32,28 @@ export const SectionItemDialog = props => {
       value: 2
     },
     {
-      label: "文章收藏夹",
+      label: "文章合集",
       value: 3
     },
     {
-      label: "资源收藏夹",
+      label: "资源合集",
       value: 4
     },
     {
-      label: "图书收藏夹",
+      label: "图书合集",
       value: 5
+    },
+    {
+      label: "文章合集展开",
+      value: 6
+    },
+    {
+      label: "资源合集展开",
+      value: 7
+    },
+    {
+      label: "图书合集展开",
+      value: 8
     }
   ];
   return (
